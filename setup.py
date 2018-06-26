2f965cca184a121b501b0c26125302a931003b90
Charlie <Charlie.frank72@gmail.com>
2018-06-26 15:05:16 -0400

    v0.0.1 - Added setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='cpc',
+      version='0.0.1',
+      author='Charles Frank',
+      author_email='Charlie.frank72@gmail.com',
+      description='Easy to use interface to look up crypto-currency prices.',
+      url='https://github.com/CharlesFrank/CryptoCurrencyCLT',
+      requires=['clint', 'requests'])
